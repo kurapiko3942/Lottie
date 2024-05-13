813e776cf86b88db67b9730e95cb620a6a93206a
Kouki Kanayama <kouki@kurapiko.com>
2024-05-13 14:14:59 +0900

    Initial Commit

---
+++ Seminar_Demo/Seminar_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seminar_DemoApp.swift
+//  Seminar_Demo
+//
+//  Created by 金山功樹 on 2024/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Seminar_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
